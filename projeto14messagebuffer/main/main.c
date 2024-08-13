e226552d3c23517dfa471c2ab8d16cf08e5cd1a1
brunomelao <you@example.com>
2024-08-13 11:07:19 -0300

    atualização

--- projeto14messagebuffer/main/main.c
+++ projeto14messagebuffer/main/main.c
@@ -18,7 +18,7 @@ void app_main(void)
     gpio_reset_pin(BT_IO);
     gpio_set_direction(BT_IO, GPIO_MODE_INPUT);
 
-    buffer = xStreamBufferCreate(25, 5);
+    buffer = xMessageBufferCreate(100);
 
     xTaskCreate(vTask1, "Task 1", 2048, NULL, 2, NULL);
     xTaskCreate(vTask2, "Task 2", 2048, NULL, 1, NULL);
@@ -28,13 +28,15 @@ void vTask1(void* pvParameters)
 {   
     int cnt = 0;
     size_t bytes_env;
+    char msg[50];
     while(1)
     {
         if(gpio_get_level(BT_IO) == 0)
         {
             cnt++;
-            bytes_env=xStreamBufferSend(buffer, &cnt, sizeof(cnt), portMAX_DELAY);
-            ESP_LOGI("TASK1", "%d bytes enviados. CNT = %d", bytes_env, cnt);
+            sprintf(msg,"Valor do cnt = %d.",cnt);
+            bytes_env = xMessageBufferSend(buffer, &msg, strlen(msg), portMAX_DELAY);
+            ESP_LOGI("TASK1", "%d bytes enviados.  MSG = %s",bytes_env,msg);
         }
         vTaskDelay(pdMS_TO_TICKS(200));
     }
@@ -42,11 +44,11 @@ void vTask1(void* pvParameters)
 
 void vTask2(void* pvParameters)
 {
-    uint8_t data;
+    char data[100];
     size_t bytes_rec;
     while(1)
     {
-        bytes_rec = xStreamBufferReceive(buffer, &data, sizeof(data), portMAX_DELAY);
-        ESP_LOGI("TASK2","%d bytes recebidos, DATA = %d", bytes_rec, data);
+        bytes_rec = xMessageBufferReceive(buffer, data, sizeof(data), portMAX_DELAY);
+        ESP_LOGI("TASK2","%d bytes recebidos, DATA = %s", bytes_rec, data);
     }
 }
