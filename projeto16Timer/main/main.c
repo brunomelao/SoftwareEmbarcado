f5cf59b6b8b68200ce352c18535589ec6dceefb3
brunomelao <you@example.com>
2024-08-20 10:53:35 -0300

    Aula Timer

---
+++ projeto16Timer/main/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <freertos/timers.h>
+#include <esp_log.h>
+#include <driver/gpio.h>
+
+void app_main(void)
+{
+
+}
